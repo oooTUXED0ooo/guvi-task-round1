f362a9ff4cfc0c303fbc0785f00332dffb1d2504
root <root@DESKTOP-Q0NNCOK>
2023-04-22 13:56:26 +0530

    added new file to develop

--- script.sh
+++ script.sh
@@ -11,12 +11,13 @@ read var2
 if (( $var1 == $var2 ))
 then
 	tar -cvzf new.tar.gz *
+	git checkout master |
 	git add new.tar.gz
 	git commit -m "added new file to master"
 	git push origin master
 else
-	git add .
-	git checkout -b develop master
+	git checkout develop
+       	git add .
 	git commit -m "added new file to develop"
 	git push origin develop
 fi
